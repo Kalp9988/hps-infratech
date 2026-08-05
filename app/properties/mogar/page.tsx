a526411c3e2417881ef58c2cbeca354ead263c81
Kalp Patel <patelkalp465@gmail.com>
2026-08-05 12:27:33 +0530

    Fix deployment error

---
+++ app/properties/mogar/page.tsx
@@ -0,0 +1,9 @@
+export default function MogarPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        HPS Farm Mogar
+      </h1>
+    </main>
+  );
+}
